34ad082000aa89cbda9bc9459c5a2bf6c39a543a
Pawel Raniowski <pawelran1998@gmail.com>
2020-10-20 15:57:10 +0200

    Added all frontend files

---
+++ src/Planer/Components/Lines/CustomLine.tsx
@@ -0,0 +1,24 @@
+import { Layer, Line, Stage, Text } from "react-konva";
+import Konva from 'konva'
+import React, { useEffect } from 'react';
+import params from '../Grid/GridConstants'
+import { snapSize } from "../../Constants/SnapConstatns";
+
+interface LineProps extends Konva.LineConfig{
+    snapToGrid: Boolean
+
+}
+export default function CustomLine(props: LineProps)
+{
+
+function snapToGrid(points: number[]) : number[]
+{
+    let startX = Math.round(points[0] / snapSize) * snapSize;
+    let startY = Math.round(points[1] / snapSize) * snapSize;
+    let endX = Math.round(points[2] / snapSize) * snapSize;
+    let endY = Math.round(points[3] / snapSize) * snapSize;
+    return [startX, startY, endX, endY];
+}
+
+return (<Line key = {props.key} points = {props.snapToGrid? snapToGrid(props.points): props.points} stroke = {props.stroke}  />);
+}
