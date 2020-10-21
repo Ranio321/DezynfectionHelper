bbb613d6655af5f09d75281b40d7e4c8ca52a343
Pawel Raniowski <pawelran1998@gmail.com>
2020-10-21 22:12:19 +0200

    changed tables to objects

--- src/Planer/Components/Sidebar/Sidebar.tsx
+++ src/Planer/Components/Sidebar/Sidebar.tsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { items } from "./SidebarItems/Items";
 import "./Sidebar.scss";
 import SidebarAddItem from "./SidebarItems/SidebarAddItem";
+import ItemProperties from "./SidebarItems/ItemProperties";
+import { Item } from "../../pointsModels";
 interface SidebarProps {
     setItem: (x: string)=> any
+    selectedItem?: Item
+    onWallDelete: (id: number) => any
 }
 
 export default function Sidebar(props: SidebarProps): JSX.Element {
@@ -19,6 +23,10 @@ export default function Sidebar(props: SidebarProps): JSX.Element {
         <div id="items">
             {collapseItems && <SidebarAddItem items = {items} setItem ={props.setItem}/>}
         </div>
+        {props.selectedItem 
+          &&
+         <ItemProperties item = {props.selectedItem} onWallDelete = {props.onWallDelete}/>}
+        
     </div>
   );
 }
--- src/Planer/Components/Sidebar/SidebarItems/ItemProperties.tsx
+++ src/Planer/Components/Sidebar/SidebarItems/ItemProperties.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Item } from '../../../pointsModels'
+import './ItemProperties.scss'
+import '../Sidebar.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 interface ItemPropertiesProps{
-
+    item?: Item
+    onWallDelete: (id: number) => any
 }
 
 export default function ItemProperties(props:ItemPropertiesProps): JSX.Element
 {
-return (<div></div>)
+    const {item} = props;
+return (<><div
+    id = "sidebarButton">
+      Item properties</div>
+<div id = "itemProperties">
+    <div id = "text"> X1</div>
+    <input id = "input" value = {item?.position.start.x}></input>
+    <div id = "text" > Y1</div>
+    <input id = "input" value = {item?.position.start.y}></input>
+    <div id = "text"> X2</div>
+    <input id = "input" value = {item?.position.end.x}></input>
+    <div id = "text"> Y2</div>
+    <input id = "input" value = {item?.position.end.y}></input>
+    <br/>
+    <FontAwesomeIcon className = "trash" icon={faTrashAlt} size="lg" onClick = {() => {props.onWallDelete(props.item?.id!)}}/>
+</div>
+</>)
 }
