00a206cfa84f44cbf406dbbfb7b2834f049e11a1
Pawel Raniowski <pawelran1998@gmail.com>
2020-10-27 21:37:47 +0100

    Stretched planer component to fit whole website

--- src/Planer/Planer.tsx
+++ src/Planer/Planer.tsx
@@ -1,11 +1,11 @@
-import React, { Ref, useEffect, useRef, useState } from "react";
+import React, {useEffect, useRef, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import PlanCanvas from "./Components/Canvas/PlanCanvas";
 import OptionsSidebar from "./Components/OptionsSidebar/OptionsSidebar";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { itemList } from "./Components/Sidebar/SidebarItems/Items";
 import { usePlaner } from "./Hooks/usePlaner";
-import { Item} from "./PlanerTypes";
+import './Planer.scss';
 interface PlanerProps {
 }
 
@@ -27,29 +27,54 @@ export default function Planer(props: PlanerProps): JSX.Element {
   }, [itemToAdd]);
 
   useEffect(() => {
+
+    function handleResize(){
     let size;
     if(canvasRef.current && canvasRef)
     {
     size = {width: canvasRef.current.offsetWidth, height: canvasRef.current.offsetHeight};
     setCanvasSize({...size});
     }
+
+  }
+    handleResize();
+    window.addEventListener('resize', handleResize)
   },[window.innerWidth, window.innerHeight])
 
   useEffect(()=>{
     setCurrentItemId(undefined);
   },[planerItems])
 
+  const cStyle = {
+    height: window.innerHeight,
+    padding: "0px",
+    margin : "0px"
+  } 
+  const colStyle = {
+    height: window.innerHeight,
+    maxWidth: "83%",
+    minWidth: "83%"
+  }
+  const optionsStyle = {
+    maxWidth: "3%"
+  }
+const sideBarStyle = {
+  maxWidth: "14%"
+}
+
+
+
   return (
-    <Container fluid>
-      <Row noGutters>
-        <Col lg="2">
+    <Container fluid  style = {cStyle}>
+      <Row noGutters >
+        <Col style = {sideBarStyle}>
           <Sidebar
             setItem={setItemToAdd}
             selectedItem={services.getItem(currentItemId)}
             onWallDelete={services.deleteItem}
           />
         </Col>
-        <Col lg="9" ref = {canvasRef}>
+        <Col ref = {canvasRef} style = {colStyle}>
           <div tabIndex={0} onKeyDown={(e) => onKeyDown(e)}>
             <PlanCanvas
               width={canvasSize.width}
@@ -61,8 +86,8 @@ export default function Planer(props: PlanerProps): JSX.Element {
               addItem = {services.addItem}
             />
           </div>
-        </Col>
-        <Col md="1">
+          </Col>
+          <Col style = {optionsStyle}>
           <OptionsSidebar undo = {services.deleteLast} delete = {services.deleteAll}/>
         </Col>
       </Row>
